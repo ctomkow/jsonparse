f915078c1bba1a3fd21f1dc8c7bc734e247ba158
Craig Tomkow <ctomkow@gmail.com>
2018-10-17 08:51:30 -0700

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='json-search',
+    version='0.1',
+    url='https://github.com/ctomkow/json-search',
+    author='Craig Tomkow',
+    author_email='craig.tomkow@gmail.com',
+    license='MIT',
+    packages=['json-search'],
+    zip_safe=False
+)
