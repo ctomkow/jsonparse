b82fb8bbc3fc3406d3c4cd37cb7b388992eaa141
Craig Tomkow <ctomkow@gmail.com>
2022-09-06 22:36:07 -0700

    fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(os.path.dirname(jsonparse.__file__), 'VERSION')) as versi
 # Package meta-data.
 NAME = 'jsonparse'
 DESCRIPTION = 'Search through JSON data for what you need'
-URL = url='https://github.com/ctomkow/jsonparse'
+URL = url = 'https://github.com/ctomkow/jsonparse'
 EMAIL = 'ctomkow@gmail.com'
 AUTHOR = 'Craig Tomkow'
 REQUIRES_PYTHON = '>=3.6.0'
