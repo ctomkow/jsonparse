612a8626a40535f14f40ae41339fc4fae16f278d
Craig Tomkow <ctomkow@gmail.com>
2022-09-15 13:22:11 -0700

    fix type checking

--- jsonparse/parser.py
+++ jsonparse/parser.py
@@ -3,7 +3,6 @@
 # Dynamically parse JSON objects
 
 # python imports
-from types import NoneType
 from typing import Union
 
 
@@ -147,7 +146,7 @@ class Parser:
     def find_key_value(self,
                        data: Union[dict, list],
                        key: str,
-                       value: Union[str, int, float, bool, NoneType]) -> list:
+                       value: Union[str, int, float, bool, None]) -> list:
         """
         Search JSON data that consists of key:value pairs for all instances of
         provided key and value pair. The parent set that contains the key:value
@@ -255,14 +254,14 @@ class Parser:
     def _stack_all_key_and_value_in_dict(
             self,
             key: str,
-            value: Union[str, int, float, bool, NoneType],
+            value: Union[str, int, float, bool, None],
             elem: dict) -> bool:
 
         if type(elem) is not dict:
             raise TypeError
         elif type(key) is not str:
             raise TypeError
-        elif not isinstance(value, (str, int, float, bool, NoneType)):
+        elif not isinstance(value, Union[str, int, float, bool, None]):
             raise TypeError
 
         if len(elem) <= 0:  # don't want an empty dict on the stack
@@ -395,7 +394,7 @@ class Parser:
             self,
             data: Union[dict, list],
             key: str,
-            value: Union[str, int, float, bool, NoneType]) -> bool:
+            value: Union[str, int, float, bool, None]) -> bool:
 
         if not isinstance(data, (dict, list)):
             raise TypeError
@@ -403,6 +402,6 @@ class Parser:
             raise TypeError
         elif not key:  # if key is an empty string
             raise ValueError
-        elif not isinstance(value, (str, int, float, bool, NoneType)):
+        elif not isinstance(value, Union[str, int, float, bool, None]):
             raise TypeError
         return True
