11715f3ef4d9182acf0e5bf0ca65a60bbab821c9
Craig Tomkow <ctomkow@gmail.com>
2022-10-16 21:24:05 -0700

    wip cli interface

--- jsonparse/cli.py
+++ jsonparse/cli.py
@@ -2,55 +2,107 @@
 # Craig Tomkow
 # 2022-10-03
 
+# local imports
+from typing import Any
+from parser import Parser
+
 # python imports
 import sys
 import argparse
 import io
+import os
+import json
 
 
 class Cli:
 
     def __init__(self):
 
-        args = self.parse_args('1.0.0')
-        self._input(args.FILE)
+        # read from the VERSION file
+        with open(os.path.join(
+                os.path.dirname(__file__), 'VERSION')) as version_file:
+            version = version_file.read().strip()
+
+        args = self.parse_args(version)
+        data = self._pythonify(self._input(args.FILE))
+
+        if 'KEY' in args:
+            print(self._jsonify(Parser().find_key(data, args.KEY[0])))
+        elif 'KEYS' in args:
+            print(self._jsonify(Parser().find_keys(data, args.KEYS, group=args.ungroup)))
+        elif 'KEYCHAIN' in args:
+            print(self._jsonify(Parser().find_key_chain(data, args.KEYCHAIN)))
+        elif ('KVKEY' in args) and ('KVVALUE' in args):
+            print(args)
+            try:
+                value = self._pythonify(args.KVVALUE[0])
+            except json.decoder.JSONDecodeError:
+                print('value is not valid json. example valid types: \\"value\\", 5, false, true, null')
+                raise SystemExit(0)
+            print(self._jsonify(Parser().find_key_value(data, args.KVKEY[0], value)))
 
     def parse_args(self, version: str) -> argparse.Namespace:
 
         parser = argparse.ArgumentParser(
             prog='jsonparse',
             formatter_class=argparse.RawDescriptionHelpFormatter,
-            description="""\
-Parse deeply nested json based on key(s) and value(s).
+            description='Parse deeply nested json based on key(s) and value(s)')
 
-With no FILE, read standard input.""")
         # all flags here
         parser.add_argument(
-            'FILE', type=argparse.FileType('r'), nargs='?',
-            default=sys.stdin, help="json file to read as input")
-        parser.add_argument(
             '-v', '--version', action='version', version=f"%(prog)s {version}")
-        group = parser.add_mutually_exclusive_group()
-        # TODO: decide if I want --key AND --keys or just --key x --key y --key z
-        # TODO: also, how to do, --keys asdf fdsa zzzz
-        group.add_argument(
-            '--key', action='store', metavar='KEY', type=str,
-            help="search for KEY")
-        group.add_argument(
-            '--keys', action='append', metavar='KEYS', type=str,
-            help="search for KEYS")
-        group.add_argument(
-            '--key-chain', action='append', metavar='KEY-CHAIN', type=str,
-            help="search for ordered KEY-CHAIN")
-        group.add_argument(
-            '--key-value', action='append', metavar='KEY-VALUE', type=str,
-            help="search for KEY:VALUE pair returning the containing set")
+
+        sub_parser = parser.add_subparsers()
+
+        key = sub_parser.add_parser('key', description='With no FILE, read standard input.')
+        key.add_argument('KEY', action='store', type=str, nargs=1, help='search for key')
+        key.add_argument(
+            'FILE', type=argparse.FileType('r'), nargs='?',
+            default=sys.stdin, help="json file or stdin to read as input")
+
+        # TODO: how to read from FILE when you specify variable number of keys? ...
+        keys = sub_parser.add_parser('keys', description='With no FILE, read standard input.')
+        keys.add_argument('--ungroup', action='store_false', help='return a one dimensional list')
+        keys.add_argument('KEYS', metavar='KEY', action='store', type=str, nargs='+', help='search for keys')
+        keys.add_argument(
+            'FILE', type=argparse.FileType('r'), nargs='?',
+            default=sys.stdin, help="json file or stdin to read as input")
+
+        # TODO: how to read from FILE when you specify variable number of keys? ...
+        key_chain = sub_parser.add_parser('key-chain', description='With no FILE, read standard input.')
+        key_chain.add_argument('KEYCHAIN', metavar='KEY', action='store', type=str, nargs='+', help='search for key chain')
+        key_chain.add_argument(
+            'FILE', type=argparse.FileType('r'), nargs='?',
+            default=sys.stdin, help="json file or stdin to read as input")
+
+        key_chain = sub_parser.add_parser('key-value', description='With no FILE, read standard input.')
+        key_chain.add_argument('KVKEY', metavar='KEY', action='store', type=str, nargs=1,
+                               help='search for key part of key:value')
+        key_chain.add_argument('KVVALUE', metavar='VALUE', action='store', type=str, nargs=1,
+                               help='must be valid json. For a string, must have escaped double quotes. e.g. \\"asdf\\"')
+        key_chain.add_argument(
+            'FILE', type=argparse.FileType('r'), nargs='?',
+            default=sys.stdin, help="json file or stdin to read as input")
+
         return parser.parse_args()
 
-    def _input(self, fp: io.TextIOWrapper):
+    def _input(self, fp: io.TextIOWrapper) -> str:
 
+        data = ''
         for line in fp:
-            print(line.rstrip())
+            data += line.rstrip()
+        return data
+
+    def _pythonify(self, data: json) -> Any:
+
+        try:
+            return json.loads(data)
+        except json.decoder.JSONDecodeError:
+            raise
+
+    def _jsonify(self, data: Any) -> json:
+
+        return json.dumps(data)
 
 
 if __name__ == "__main__":
