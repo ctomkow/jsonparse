a08191ec653cd8a96a11d8532821f5b5ce76cf4a
ctomkow <ctomkow@gmail.com>
2023-10-01 19:58:01 -0600

    fix?

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         ],
         extras_require={
           'webapi': [
-                        'flask==2.0.3',
+                        'flask==3.0.0',
                         'gunicorn==20.1.0'
                     ]
         },
