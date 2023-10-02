b2684d6fe5d7832a7d1107f55823ae8a751918fb
ctomkow <ctomkow@gmail.com>
2023-10-01 19:50:52 -0600

    fix ci?

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         extras_require={
           'webapi': [
                         'flask==2.0.3',
-                        'gunicorn>=20.1.0,<21.0.0'
+                        'gunicorn==20.1.0'
                     ]
         },
         entry_points={
