0ba2e13e6282c2ea073b3d092669fcd9c032db53
ctomkow <ctomkow@gmail.com>
2024-04-17 16:43:41 -0600

    wip find_value with tests. finish adding to cli and webapi and functions modules"

--- jsonparse/parser.py
+++ jsonparse/parser.py
@@ -245,6 +245,34 @@ class Parser:
 
         return value_list
 
+    def find_value(self, data: Union[dict, list], value: Union[str, int, float, bool, None]) -> list:
+        # todo: docstrings
+        if not self._valid_value_input(data, value):
+            raise
+
+        self.stack_ref = self._stack_init()  # init a new stack every request
+        self._stack_push(data)
+        self._stack_trace()
+
+        key_list = []
+
+        while self._stack_size() >= 1:
+
+            elem = self._stack_pop()
+
+            if type(elem) is list:
+                self._stack_push_list_elem(elem)
+            elif type(elem) is dict:
+                key = self._stack_all_value_in_dict(value, elem)
+                if key:
+                    key_list.insert(0, key)
+            else:  # according to RFC 7159, valid JSON can also contain a
+                # string, number, 'false', 'null', 'true'
+                pass  # discard these other values as they don't have a key
+
+        return key_list
+
+
     # STACK operations
 
     def _stack_init(self) -> list:
@@ -348,6 +376,24 @@ class Parser:
             for e in elem:
                 if e == key and elem[e] == value:
                     return True
+                else:
+                    self._stack_push(elem[e])
+                    self._stack_trace()
+        return False
+
+    def _stack_all_value_in_dict(self, value: Union[str, int, float, bool, None], elem: dict) -> str:
+
+        if type(elem) is not dict:
+            raise TypeError
+        elif not isinstance(value, (str, int, float, bool, type(None))):
+            raise TypeError
+
+        if len(elem) <= 0:  # don't want an empty dict on the stack
+            pass
+        else:
+            for e in elem:
+                if elem[e] == value:
+                    return e
                 else:
                     self._stack_push(elem[e])
                     self._stack_trace()
@@ -496,6 +542,14 @@ class Parser:
             raise TypeError
         elif not key:  # if key is an empty string
             raise ValueError
+        elif not isinstance(value, (str, int, float, bool, type(None))):
+            raise TypeError
+        return True
+
+    def _valid_value_input(self, data: Union[dict, list], value: Union[str, int, float, bool, None]) -> bool:
+
+        if not isinstance(data, (dict, list)):
+            raise TypeError
         elif not isinstance(value, (str, int, float, bool, type(None))):
             raise TypeError
         return True
